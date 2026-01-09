a8e3ac413a3436d00afd9cd9a3dc2b445928df95
Michal Fousek <chlup@chlup.info>
2026-01-09 12:24:07 +0100

    Bump Android app version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.mugeaters.popelnice.nvpp"
         minSdk = 26
         targetSdk = 35
-        versionCode = 14
-        versionName = "1.5"
+        versionCode = 15
+        versionName = "1.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
